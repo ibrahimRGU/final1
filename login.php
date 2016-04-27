02ba6562341ec45ac7b23b05f04ba61a74dc5261
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-04-27 15:45:16 +0100

    Update login.php

--- login.php
+++ login.php
@@ -3,6 +3,30 @@
 	include("connection.php"); //Establishing connection with our database
 	
 	$error = ""; //Variable for storing our errors.
+	//new lockout code
+	$bad_login_limit = 3;
+	$lockout_time = 600;
+	$first_failed_login, failed_login_count; // retrieve from DB
+	if(
+		 ($failed_login_count >= $bad_login_limit)
+    		&&
+    		(time() - $first_failed_login < $lockout_time)
+		) {
+	 	echo "You are currently locked out.";
+  		exit; // or return, or whatever.
+		} else if( /* login is invalid */ ) {
+		 if( time() - $first_failed_login > $lockout_time ) {
+	 // first unsuccessful login since $lockout_time on the last one expired
+	  $first_failed_login = time(); // commit to DB
+	  $failed_login_count = 1; // commit to db
+	 } else {
+	  $failed_login_count++; // commit to db.
+	  }
+	  exit; // or return, or whatever.
+	} else 
+	 // user is not currently locked out, and the login is valid.
+	 // do stuff
+	
 	if(isset($_POST["submit"]))
 	{
 		if(empty($_POST["username"]) || empty($_POST["password"]))
