32bd9345b3ccbecb4a36cdf3aa6901f49e726927
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-04-27 16:28:45 +0100

    Update login.php

--- login.php
+++ login.php
@@ -15,6 +15,37 @@ if(
 ) {
   echo "You are currently locked out.";
   exit; // or return, or whatever.
+  
+  if(isset($_POST["submit"]))
+	{
+		if(empty($_POST["username"]) || empty($_POST["password"]))
+		{
+			$error = "Both fields are required.";
+		}else
+		{
+			// Define $username and $password
+			$username=$_POST['username'];
+			$password=$_POST['password'];
+			
+			//Check username and password from database
+			$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
+			$result=mysqli_query($db,$sql);
+			$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
+			
+			//If username and password exist in our database then create a session.
+			//Otherwise echo error.
+			
+			if(mysqli_num_rows($result) == 1)
+			{
+				$_SESSION['username'] = $username; // Initializing Session
+				header("location: photos.php"); // Redirecting To Other Page
+			}else
+			{
+				$error = "Incorrect username or password.";
+			}
+		}
+	}
+  
 } else if( /* login is invalid */ ) {
   if( time() - $first_failed_login > $lockout_time ) {
     // first unsuccessful login since $lockout_time on the last one expired
