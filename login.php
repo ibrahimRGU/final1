6942f38e01b21993b83ed2804893c588fb201978
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-04-27 22:25:48 +0100

    Update login.php

--- login.php
+++ login.php
@@ -2,21 +2,8 @@
 	session_start();
 	include("connection.php"); //Establishing connection with our database
 	
-	
-$bad_login_limit = 3;
-$lockout_time = 600;
-
-$first_failed_login, failed_login_count; // retrieve from DB
-
-if(
-    ($failed_login_count >= $bad_login_limit)
-    &&
-    (time() - $first_failed_login < $lockout_time)
-) {
-  echo "You are currently locked out.";
-  exit; // or return, or whatever.
-  
-  if(isset($_POST["submit"]))
+	$error = ""; //Variable for storing our errors.
+	if(isset($_POST["submit"]))
 	{
 		if(empty($_POST["username"]) || empty($_POST["password"]))
 		{
@@ -43,26 +30,6 @@ if(
 			{
 				$error = "Incorrect username or password.";
 			}
-		 else if( /* login is invalid */ ) {
-  if( time() - $first_failed_login > $lockout_time ) {
-    // first unsuccessful login since $lockout_time on the last one expired
-    $first_failed_login = time(); // commit to DB
-    $failed_login_count = 1; // commit to db
-  } else {
-    $failed_login_count++; // commit to db.
-  }
-  exit; // or return, or whatever.
-} else {
-  // user is not currently locked out, and the login is valid.
-  // do stuff
-	header("location: photos.php");
-}
-
-			
 		}
-		
 	}
-	
-
-
 ?>
