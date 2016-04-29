899651af0a8a6064a6cc5435fdcafc1072fcf35f
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-04-29 08:31:04 +0100

    Update search.php

--- search.php
+++ search.php
@@ -3,6 +3,10 @@ $resultText = "";
 if(isset($_POST["submit"]))
 {
     $name = $_POST["username"];
+    
+    // To protect from MySQL injection
+    $name = stripslashes($username);
+	$name = mysqli_real_escape_string($db, $username);
 
     $sql="SELECT userID FROM users WHERE username='$name'";
     $result=mysqli_query($db,$sql);
