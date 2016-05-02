0d76a8614efd8ceb807f005e59c4d04ef77d8cd6
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-05-02 02:42:52 +0100

    Update check.php

--- check.php
+++ check.php
@@ -1,4 +1,4 @@
-<?php
+</*?php
 include('connection.php');
 session_start();
 $user_check=$_SESSION['username'];
@@ -15,5 +15,46 @@ if($row['admin']==1){
 if(!isset($user_check))
 {
 header("Location: index.php");
+}*/
+
+
+<?php
+include('connection.php');
+session_start();
+{
+	//check session highjacking
+if (!($ip==$_SERVER['REMOTE_ADDR'])){
+    header("location: logout.php"); // Redirecting To Other Page
 }
+
+//check logut/idle time
+if($_SESSION ["timeout"]+60 < time()){
+
+    //session timed out
+    header("location: logout.php"); // Redirecting To Other Page
+}else{
+    //reset session time
+    $_SESSION['timeout']=time();
+}
+}
+
+$user_check=$_SESSION['username'];
+
+$ses_sql = mysqli_query($db,"SELECT username, admin FROM users WHERE username='$user_check' ");
+
+$row=mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+
+$login_user=$row['username'];
+if($row['admin']==1){
+    $adminuser = true;
+}
+
+if(!isset($user_check))
+{
+header("Location: index.php");
+}
+?>
+
+
+
 ?>
