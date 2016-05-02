7017ac69112174d38b9f628a9ea8167c57bbc7be
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-05-02 03:00:38 +0100

    Update check.php

--- check.php
+++ check.php
@@ -20,9 +20,6 @@ header("Location: index.php");
 
 
 <?php
-include('connection.php');
-session_start();
-{
 	//check session highjacking
 if (!($ip==$_SERVER['REMOTE_ADDR'])){
     header("location: logout.php"); // Redirecting To Other Page
@@ -37,25 +34,4 @@ if($_SESSION ["timeout"]+20 < time()){
     //reset session time
     $_SESSION['timeout']=time();
 }
-}
-
-$user_check=$_SESSION['username'];
-
-$ses_sql = mysqli_query($db,"SELECT username, admin FROM users WHERE username='$user_check' ");
-
-$row=mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-
-$login_user=$row['username'];
-if($row['admin']==1){
-    $adminuser = true;
-}
-
-if(!isset($user_check))
-{
-header("Location: index.php");
-}
-?>
-
-
-
 ?>
