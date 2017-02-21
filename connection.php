10d024aa9b401d5ebae3366f3e972b3ef7c1d3ee
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2017-02-21 03:03:35 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
-define('DB_USERNAME', 'b307281dd0861c');
-define('DB_PASSWORD', '6f7d250a');
-define('DB_DATABASE', 'hackerton');
+define('DB_USERNAME', 'bfd67c836b0287');
+define('DB_PASSWORD', '679c1a2e');
+define('DB_DATABASE', 'samsung1516312');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
