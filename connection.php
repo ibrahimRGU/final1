94a4b9bc1931842861f07292539191c5bbb68abd
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2017-02-21 03:01:32 +0000

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME', 'b307281dd0861c');
+define('DB_PASSWORD', '6f7d250a');
+define('DB_DATABASE', 'hackerton');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
