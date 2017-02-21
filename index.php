8287b9b39e5d9b29b0bbdb9202ccc660535f15f4
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2017-02-21 02:58:29 +0000

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if(isset($_SESSION["username"])) {
     <head>
         <meta charset="UTF-8">
         <title>Login Page</title>
-        <link rel="stylesheet" type="text/css" href="assets/style.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
 
