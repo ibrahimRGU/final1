dfe3d6494cffe4a6a051f222a0b3dfe190cf976d
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2017-02-21 17:01:36 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
-define('DB_USERNAME', 'bfd67c836b0287');
-define('DB_PASSWORD', '679c1a2e');
-define('DB_DATABASE', 'samsung1516312');
+define('DB_USERNAME', 'b3b72770681f66');
+define('DB_PASSWORD', 'cbca62aa');
+define('DB_DATABASE', 'code4me');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
